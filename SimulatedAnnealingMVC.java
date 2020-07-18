01cd08d21b98c42a7bb5d6f02e8df51f47f647ab
Bianca Ferreira da Frota Barreto <bbarreto8150@gmail.com>
2020-07-17 22:04:39 -0400

    César's changes

--- SimulatedAnnealingMVC.java
+++ SimulatedAnnealingMVC.java
@@ -17,7 +17,6 @@ public class SimulatedAnnealingMVC {
 
   public void initialize(int[][] matrix, int vertices, int edges, double t_max, double k, double kt, double t_min) {
     this.matrix = new int[matrix.length][matrix.length];
-    copyMatrix(matrix, this.matrix);
     this.matrix_length = matrix.length;
     this.vertices = vertices;
     this.edges = edges;
@@ -26,6 +25,7 @@ public class SimulatedAnnealingMVC {
     this.kt = kt;
     this.t_min = t_min;
 
+    copyMatrix(matrix, this.matrix);
     simulate_anneal();
   }
 
@@ -43,31 +43,31 @@ public class SimulatedAnnealingMVC {
 
     double T = this.t_max;
     double T_min = this.t_min;
-    String vertices_mvc_solution = generate_course(this.matrix, this.edges);
-    int aval_cover = avaluate_cover(vertices_mvc_solution);
+    String vertices_mvc_solution_VC = generate_course(this.matrix, this.edges);
+    int aval_cover_VC = avaluate_cover(vertices_mvc_solution_VC);
+    int aval_cover_VN;
 
-    // while (T >= T_min) {
-    // while (i < this.kt) {
-    // int[] neighboorhood = generate_neighboor(course);
-    // double aval_vn = avaluate_cover(neighboorhood, this.matrix_length);
+    while (T >= T_min) {
+      while (i < this.kt) {
+        String vertices_mvc_solution_VN = generate_course(this.matrix, this.edges);
+        aval_cover_VN = avaluate_cover(vertices_mvc_solution_VN);
 
-    // if (aval_vn < aval_cover) {
-    // course = neighboorhood;
-    // } else {
-    // double e = (aval_cover - aval_vn) / T;
-    // if (random.nextInt(1) < Math.exp(e)) {
-    // course = neighboorhood;
-    // }
-    // }
+        if (aval_cover_VC < aval_cover_VN) {
+          vertices_mvc_solution_VC = vertices_mvc_solution_VN;
+        } else {
+          double e = (aval_cover_VN - aval_cover_VC) / T;
+          if (random.nextInt(1) < Math.exp(e)) {
+            vertices_mvc_solution_VC = vertices_mvc_solution_VN;
+          }
+        }
 
-    // i = i + 1;
-    // }
+        i = i + 1;
+      }
 
-    // T = this.k * T;
-    // t = t + 1;
-
-    System.out.println("\n\nCobertura mínima encontrada: " + avaluate_cover(vertices_mvc_solution));
-    // }
+      T = this.k * T;
+      t = t + 1;
+      System.out.println("\n\nCobertura mínima encontrada é a solução : " + aval_cover_VN);
+    }
   }
 
   public int avaluate_cover(String vertices_solution) {
@@ -85,64 +85,74 @@ public class SimulatedAnnealingMVC {
     int i = 0;
     int j = 0;
     int current_vertex = 0;
+    int melhorLocal_i = 0;
+    int melhorLocal_j = 0;
 
     copyMatrix(matrix, course_matrix);
 
     while (edges_count > 0) {
       /* Choose random edge */
       while (first_vertex == 0) {
-        i = random.nextInt(course_matrix_length - 1);
-        j = random.nextInt(course_matrix_length - 1);
-        System.out.println("course_matrix_length: " + course_matrix_length);
-        // System.out.println("i: " + i);
-        // System.out.println("j: " + j);
+        i = random.nextInt(course_matrix_length);
+        j = random.nextInt(course_matrix_length);
+        System.out.println("vertece i: " + i);
+        System.out.println("vertice  j: " + j);
 
         first_edge[0] = i;
-        System.out.println("first_edge[0: " + first_edge[0]);
         first_edge[1] = j;
-        System.out.println("first_edge[1: " + first_edge[1]);
 
         first_vertex = course_matrix[i][j];
         System.out.println("first_vertex: " + first_vertex);
-        for (int x = 0; x < course_matrix_length; x++) {
-          for (int y = 0; y < course_matrix_length; y++) {
-            System.out.print(matrix[i][j] + " ");
-          }
-          System.out.println();
-        }
-        System.out.println("first_vertex: " + first_vertex);
       }
       first_vertex = 0;
+
       /* Choosing random vertex from first_edge */
-      current_vertex = first_edge[random.nextInt(1)];
+      melhorLocal_i = 0;
+      melhorLocal_j = 0;
+      for (int a = 0; a < course_matrix_length; a++) {
+        if (course_matrix[first_edge[0]][a] == 1) {
+          melhorLocal_i = melhorLocal_i + 1;
+        }
+        if (course_matrix[first_edge[1]][a] == 1) {
+          melhorLocal_j = melhorLocal_j + 1;
+        }
+      }
+      System.out.println("quantidade do i: " + melhorLocal_i + " quantidade do j: " + melhorLocal_j);
+
+      if (melhorLocal_i >= melhorLocal_j) {
+        current_vertex = first_edge[0];
+      } else {
+        current_vertex = first_edge[1];
+      }
+
+      // current_vertex = first_edge[random.nextInt(1)];
       System.out.println("\nfirst_edge[0]: " + first_edge[0]);
       System.out.println("\nfirst_edge[1]: " + first_edge[1]);
 
       /* Adding current vertex to MVC solution */
       min_vertices_solution = min_vertices_solution + (current_vertex + 1); // passando para index 1
-      System.out.println("2 - min_vertices_solution: " + min_vertices_solution);
+      System.out.println("vertices: " + min_vertices_solution);
 
       /* Choosing random edges from current_vertex */
       for (int k = 0; k < course_matrix_length; k++) {
         if (course_matrix[current_vertex][k] == 1) {
           course_matrix[current_vertex][k] = 0;
           edges_count = edges_count - 1;
-          System.out.println("\nREMOVENDO current_vertex: " + current_vertex);
-          System.out.println("REMOVENDO k: " + k + "\n");
+          System.out.println("\nREMOVENDO current_vertex: " + current_vertex + " e " + k);
           System.out.println("edges_count: " + edges_count + "\n");
         }
 
         if (course_matrix[k][current_vertex] == 1) {
           course_matrix[k][current_vertex] = 0;
-          System.out.println("REMOVENDO k: " + k);
-          System.out.println("REMOVENDO current_vertex: " + current_vertex + "\n");
+          System.out.println("REMOVENDO current_vertex: " + k + " e " + current_vertex);
           System.out.println("edges_count: " + edges_count + "\n");
         }
       }
       System.out.println("FINAL edges_count: " + edges_count + "\n");
     }
 
-    System.out.println("3 - min_vertices_solution: " + min_vertices_solution);
+    System.out.println("resultado final: " + min_vertices_solution);
+    System.out.println("-----------------------------------------");
     return min_vertices_solution;
   }
 
